250dec0eb6adffe50c45766ac9f8001bb9af1bf2
Jefferson <jefferson_uk@hotmail.com>
2023-06-14 22:10:46 +0200

    Create home and product components and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
-import { styled } from '../styles'
+import { HomeContainer, Product } from '@/src/styles/pages/home'
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$green500',
-  borderRadius: 4,
-})
+import shirt1 from '../assets/tshirts/1.png'
+import shirt2 from '../assets/tshirts/2.png'
 
 export default function Home() {
-  return <Button>Enviar</Button>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={shirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-Shirt 1</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={shirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>T-Shirt 2</strong>
+          <span>R$ 85,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
