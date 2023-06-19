a3bef3f10a9792b2933ebfdee609e7c809608c0b
Jefferson <jefferson_uk@hotmail.com>
2023-06-19 22:18:32 +0200

    Configure SEO headers

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Stripe from 'stripe'
 import { stripe } from '@/lib/stripe'
 import { ImageContainer, SuccessContainer } from '../styles/pages/success'
+import Head from 'next/head'
 
 interface SuccessProps {
   costumerName: string
@@ -15,24 +16,39 @@ interface SuccessProps {
 
 export default function Success({ costumerName, product }: SuccessProps) {
   return (
-    <SuccessContainer>
-      <h1>Compra efetuada</h1>
+    <>
+      <Head>
+        <title>Compra efetuada | Ignite Shop</title>
 
-      <ImageContainer>
-        <Image src={product.imageUrl} width={120} height={110} alt="" />
-      </ImageContainer>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <SuccessContainer>
+        <h1>Compra efetuada</h1>
 
-      <p>
-        Uhuul <strong>{costumerName}</strong>, sua{' '}
-        <strong>{product.name}</strong> já está a caminho da sua casa.
-      </p>
+        <ImageContainer>
+          <Image src={product.imageUrl} width={120} height={110} alt="" />
+        </ImageContainer>
 
-      <Link href="/">Voltar ao catálogo</Link>
-    </SuccessContainer>
+        <p>
+          Uhuul <strong>{costumerName}</strong>, sua{' '}
+          <strong>{product.name}</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href="/">Voltar ao catálogo</Link>
+      </SuccessContainer>
+    </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
   const sessionId = String(query.session_id)
 
   const session = await stripe.checkout.sessions.retrieve(sessionId, {
